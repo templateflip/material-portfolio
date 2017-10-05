b27999dd2f26b491c48c71e0dcdcaf40b0917a16
Kanishk Kunal <kanishk.kunal@hotmail.com>
2017-10-05 15:03:08 +0530

    Updating license and dist

--- dist.js
+++ dist.js
@@ -10,8 +10,11 @@ if (!which('7z')) {
 cd('public');
 
 var inPath = '**'// folder to zip
+
 var licenseFree = path.join(__dirname, 'license-free.txt')
+
 var licensePremium = path.join(__dirname, 'license-premium.txt')
+var sourcePremium = path.join(__dirname, 'styles/')
 
 var outPathFree = path.join(__dirname, package.name + '_free_' + package.version.split('.').join('-') + '.zip') // name of output zip file
 var outPathPremium = path.join(__dirname, package.name + '_premium_' + package.version.split('.').join('-') + '.zip') // name of output zip file
@@ -34,6 +37,12 @@ if (exec('7z a "'+ outPathFree + '" "' + licenseFree +'"').code !== 0) {
 
 // Run external tool synchronously for premium version
 if (exec('7z a "'+ outPathPremium + '" "' + inPath +'"').code !== 0) {
+  echo('Error: 7z commit failed');
+  exit(1);
+}
+
+
+if (exec('7z a "'+ outPathPremium + '" "' + sourcePremium +'"').code !== 0) {
   echo('Error: 7z commit failed');
   exit(1);
 }
